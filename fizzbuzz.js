9ffb56668ed45459ed4d6d5c365585013bb0a11f
Jorge Hassan <jhassan@ventera.com>
2020-02-10 12:57:59 -0500

    update readme and fix wording

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,9 +4,9 @@ const fizzbuzz = (m, n) => {
     if (i % 15 === 0) {
       result.push("FizzBuzz");
     } else if (i % 5 === 0) {
-      result.push("Fizz");
-    } else if (i % 3 === 0) {
       result.push("Buzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
     } else {
       result.push(i.toString());
     }
